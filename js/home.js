3ad3b86373a06e619a84f554e7c946322f411fc8
Lucia Sanchez <luciasanchezarrabal@gmail.com>
2025-02-08 18:44:59 +0100

    añadiendo carousel de categorías

--- js/home.js
+++ js/home.js
@@ -6,15 +6,14 @@ function getRandomInt(min, max) {
 
 function createCarouselImages() {
     let categories = ['mens-shirts','mens-shoes','mens-watches','womens-bags','womens-dresses','womens-jewellery','womens-shoes','womens-watches']
-    let carouselImages = document.getElementById('carouselImages');
+    let carouselImages = document.getElementById('carouselImages-products');
     carouselImages.innerHTML = ""
-    for (let i=0;i<3;i++) {
+    for (let i=0;i<5;i++) {
         let category_index = getRandomInt(0, 7)
         let category = categories[category_index]
         fetch(`https://dummyjson.com/products/category/${category}`)
         .then(res => res.json())
         .then(res => {
-            console.log(res)
             let numberOfProducts = res.products.length
             let product_index = getRandomInt(0, numberOfProducts)
             let image = res.products[product_index].images[0]
@@ -33,5 +32,46 @@ function createCarouselImages() {
         });
     }
 }
+async function getImageCategory(url) {
+    let img = await fetch(url)
+            .then(res => res.json())
+            .then(res => {return res.products[0].images[0]})
+    
+    return img
+}
+async function getCategories() {
+    let data = await fetch(`https://dummyjson.com/products/categories/`)
+    .then(res => res.json())
+    .then(res => {return res})
+    return data
+}
+async function createCarouselCategory() {
+    let carouselImages = document.getElementById('carouselImages-categories');
+    carouselImages.innerHTML = ""
+    res = await getCategories()
+    
+    for (let i=0;i<res.length;i++) {
+        let category=res[i]
+        let categoryName = category.name
+        let categoryURL = category.url
+        let categoryImg = await getImageCategory(categoryURL)
+        if (i==0) {
+            childCarousel = `<div class="carousel-item active">
+            <h3>${categoryName}</h3>
+            <img src="${categoryImg}" class="carousel-item-image" alt="${categoryName}">
+            </div>`
+        }
+        else {
+            childCarousel = `<div class="carousel-item">
+            <img src="${categoryImg}" class="carousel-item-image" alt="${categoryName}">
+            <h3>${categoryName}</h3>
+
+            </div>`
+        }
+        
+        carouselImages.innerHTML+=childCarousel
+    }
+}
 
 window.addEventListener("DOMContentLoaded", createCarouselImages);
+window.addEventListener("DOMContentLoaded", createCarouselCategory);
