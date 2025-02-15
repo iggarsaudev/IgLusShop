1fa57f129d71a7fcce7d39a8762c7f8b022e9db0
Lucia Sanchez <luciasanchezarrabal@gmail.com>
2025-02-15 18:34:51 +0100

    añadiendo estilos e imagenes a reviews

--- js/reviews.js
+++ js/reviews.js
@@ -31,8 +31,59 @@ function selectCategories() {
 }
 document.addEventListener('DOMContentLoaded',selectCategories)
 
-function mostrarReview(reviews) {
+async function generarImagen(name) {
+    let res = await fetch(`https://dummyjson.com/users/search?q=${name}`)
+    let data = await res.json()
+    console.log(data.users[0])
+    let gender = data.users[0].gender
+
+    let resRandomUser = await fetch(`https://randomuser.me/api/?gender=${gender}`)
+    let dataRandomUser = await resRandomUser.json()
+    console.log(dataRandomUser)
+    let image = dataRandomUser.results[0].picture.large
+
+   return image
+}
+
+
+
+async function crearReview(review,title) {
+    let rating = review.rating
+    let star = ""
+    let username = review.reviewerName
+    let firstName = username.split(" ")[0]
+    for (let i=0;i<rating;i++) {
+        star+=`<span class="material-icons">
+        star
+      </span>`
+    }
+    console.log(username)
+    let img = await generarImagen(firstName)
+    console.log(img)
+    let reviewCard = `<div class="review">
+    <div class="review_left">
+        <span class="review_name">${username}</span>
+        <img class="review_img" src="${img}" alt="User image">
+        <div class="review_rate">
+            ${star}
+        </div>
+     
+    </div>
+    <div class="review_right">
+      <h2 class="review_product">${title}</h2>
+      <h3 class="review_date">${review.date}</h3>
+      <p class="review_comment">${review.comment}</p>
+    </div>
+
+
+  </div>`
+  return reviewCard
+}
+async function mostrarReview(product) {
+    console.log(product)
     const divReviews = document.getElementById('reviewsContainer')
+    const reviews = product.reviews
+    const name = product.title
 
 //     comment
 //     : 
@@ -49,14 +100,13 @@ function mostrarReview(reviews) {
 //     reviewerName
 //     : 
 // "Leah Hughes"
-    
-    reviews.forEach(
-        (e)=>{
-            let reviewCard = `<p>${e.comment}</p>`
-            divReviews.innerHTML += reviewCard
-        }
-    )
+    for (let i=0;i<reviews.length;i++) {
+        let review = reviews[i]
 
+        let reviewCard = await crearReview(review,name)
+        divReviews.innerHTML += reviewCard
+    }
+       
 }
 function mostrarAllReviews(category) {
     const divReviews = document.getElementById('reviewsContainer')
@@ -65,7 +115,6 @@ function mostrarAllReviews(category) {
     fetch(`https://dummyjson.com/products/category/${category}`)
         .then(res => res.json())
         .then(res => {
-            console.log(res.products)
-            res.products.forEach((product)=>{mostrarReview(product.reviews)})
+            res.products.forEach((product)=>{mostrarReview(product)})
         });
 }
